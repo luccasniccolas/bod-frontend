7222901aa359f24ada42b6e1ebecc4d6333243c2
Lucas Araya <lucas.araya_t@mail.udp.cl>
2024-06-11 22:49:24 -0400

    form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -38,7 +38,7 @@ function AuthForm() {
               href="#"
               className="border-2 border-green-500 text-green-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-green-500 hover:text-white"
             >
-              Registrarse
+              Iniciar Sesión
             </Link>
               </div>
             </div>
