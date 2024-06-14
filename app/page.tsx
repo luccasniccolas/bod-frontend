a5edf07dd0b3f5acd1d34437bd8512e7e523fa99
Sej0taGrove <sebastian.espinoza@mail.udp.cl>
2024-06-14 00:26:10 -0400

    modificaciones al front

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import ProductDetails from './producto/[id]';
 import VANTA from 'vanta/dist/vanta.net.min.js';
-import Navbar from './ui/Navbar';
 import Footer from './ui/Footer';
 
 export default function Home() {
   const vantaRef = useRef<HTMLDivElement>(null);
+  const [data, setData] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     const script1 = document.createElement('script');
@@ -17,10 +20,8 @@ export default function Home() {
     script2.async = true;
 
     document.body.appendChild(script1);
-
     script1.addEventListener('load', () => {
       document.body.appendChild(script2);
-
       script2.addEventListener('load', () => {
         if (vantaRef.current && window.VANTA) {
           window.VANTA.NET({
@@ -41,23 +42,43 @@ export default function Home() {
     });
 
     return () => {
-      script1.removeEventListener('load', () => {});
-      script2.removeEventListener('load', () => {});
+      script1.removeEventListener('load', () => { });
+      script2.removeEventListener('load', () => { });
     };
   }, []);
 
+  useEffect(() => {
+    fetch("http://localhost:3001/api/productos")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const handleProductClick = (product) => {
+    router.push(`/productos/${product.productoid}`, { state: { product } });
+  };
+
   return (
     <>
-      <Navbar />
-      <div ref={vantaRef} style={{ width: '100%', height: '100vh' }}>
-        {/* Contenido de tu página */}
-        <div className="welcome">
-          <h1>Bienvenidos a BOD</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dignissimos perferendis
-            ut mollitia velit dicta deserunt fugit omnis id non eos alias explicabo, totam ad illo
-            repellat reiciendis. Neque, cum!
-          </p>
+      <div className="welcome">
+        <div className="w-full">
+          <img src="/images/bod/panoramic.jpg" alt="Nueva Colección" className="w-full h-128 object-cover" />
+        </div>
+        <div ref={vantaRef} style={{ width: '100%', height: '100vh' }}>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mx-auto max-w-7xl">
+            {data.map((product) => (
+              <div
+                key={product.productoid}
+                onClick={() => handleProductClick(product)}
+                className="mt-16 mx-3 bg-white rounded-lg shadow-md p-8 cursor-pointer"
+              >
+                <div className="h-64 flex items-center justify-center">
+                  <img src={`${product.url_imagen}`} alt={product.nombre} className="object-contain h-full" />
+                </div>
+                <h2 className="text-xl font-bold mb-2">{product.nombre}</h2>
+                <p className="text-gray-700">${product.precio}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <Footer />
