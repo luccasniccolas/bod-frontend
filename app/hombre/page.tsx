5c05db236cf66aea46a2494273fc3280d1dd000c
Lucas Araya <lucas.araya_t@mail.udp.cl>
2024-06-14 00:28:30 -0400

    nada

--- app/hombre/page.tsx
+++ app/hombre/page.tsx
@@ -1,25 +1,17 @@
 "use client"
 import { useState, useEffect } from "react";
+import { fetchProducts } from "../lib/data";
 
 export default function Hombre() {
 
-  const [data, setData] = useState(null)
-
-  useEffect(() => {
-    fetch("http://localhost:3001/api/productos")
-    .then((response) => response.json())
-    .then((data) => setData(data));
-  }, []);
+  const data = fetchProducts();
+  console.log(data);
 
   return(
     <div>
       <h1>Productos</h1>
       <div>
-        <ul>
-          {data?.map((product) => (
-            <li key={product.productoid}>{product.nombre} {product.precio}</li>
-          ))}
-        </ul>
+        
       </div>
       </div>
   )
---
+++ app/lib/data.ts
@@ -0,0 +1,12 @@
+import { Product } from "./definitions";
+
+export async function fetchProducts() {
+    try {
+        const data = await fetch('http://localhost:3001/api/productos');
+        return data.json()
+    } catch(error) {
+        console.error("Fetch error:", error);
+        throw new Error('Error al hacer fetch a los productos')
+    }
+    
+}
